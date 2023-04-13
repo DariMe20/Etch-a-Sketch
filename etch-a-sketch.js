1a9e750ad21d7c5b741b4ba3f7769f783d19c621
Daria <dariamesesan@yahoo.com>
2023-04-13 15:37:06 +0300

    Added functions for change color (in black for now) and clear board

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -15,3 +15,28 @@ for (let i = 0; i < 16; i++) {
 }
 
 const cells = document.getElementsByClassName('cell'); /*the array of cells*/
+const color = document.getElementsByClassName('color');
+const CustomColor = document.getElementById('chooseColor');
+const RangeInput = document.getElementById('InputRangeId');
+const RangeOutput = document.getElementById("OutputRangeId");
+
+//Add event listeners to cell
+for(let i=0; i<cells.length; i++)
+{
+   {
+   cells[i].addEventListener('mouseover', changeColor);
+
+   //Function to change color
+   function changeColor(event, color){
+    event.target.style.backgroundColor = color.value;
+   }
+
+}
+
+// Function to reset the board
+function resetBoard() {
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].style.backgroundColor = 'white'; // Reset cell color to white
+    }
+} 
+}
