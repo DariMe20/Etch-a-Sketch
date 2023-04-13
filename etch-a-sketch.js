2a41ea37510875e7397cf006942d506ba8f825b3
Daria <dariamesesan@yahoo.com>
2023-04-13 18:15:22 +0300

    Fixed issue with saving option not working for white cells

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,7 +4,8 @@ const colorsDiv = document.getElementsByClassName('color');
 const CustomColor = document.getElementById('chooseColor');
 const RangeInput = document.getElementById('InputRangeId');
 const RangeOutput = document.getElementById("OutputRangeId");
-const saveBtn = document.getElementById("")
+const saveBtn = document.getElementById("");
+var theColor;
 
 // Add event listener to range input
 RangeInput.addEventListener('input', ChangeGridSize);
@@ -12,9 +13,10 @@ RangeInput.addEventListener('input', ChangeGridSize);
 CreateGrid(16);
 
 //function that creates a grid
-function CreateGrid(size){
+function CreateGrid(size) {
+
     //initialize empty container
-    container.innerHTML ='';
+    container.innerHTML = '';
 
     // Create new grid with specified size
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -23,41 +25,39 @@ function CreateGrid(size){
     //create cells inside grid 
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
+
             // Create div element
             const div = document.createElement('div');
-    
+
             // Add div element to class list
             div.classList.add('cell');
 
+
             // Add event listener to cell
-            div.addEventListener('mouseover', function(event){
+            div.addEventListener('mouseover', function (event) {
                 changeColor(event, theColor);
             });
-    
+
             // Add cell to container
             container.appendChild(div);
         }
-    } 
+    }
 }
-
-//Access cells -const cells = document.getElementsByClassName('cell');
-
 
 //Add event listeners for color divs
 for (let i = 0; i < colorsDiv.length; i++) {
-    colorsDiv[i].addEventListener('click', function(event){
+    colorsDiv[i].addEventListener('click', function (event) {
         theColor = event.target.dataset.clr;
     });
 }
 
 //Add event listener for custom input color
-CustomColor.addEventListener('input', function(event){
+CustomColor.addEventListener('input', function (event) {
     theColor = event.target.value;
 })
 
-
 //Function to change color
-function changeColor(event, theColor){
+function changeColor(event, theColor) {
     event.target.style.backgroundColor = theColor;
 }
 
@@ -67,53 +67,75 @@ function resetBoard() {
     for (let i = 0; i < cells.length; i++) {
         cells[i].style.backgroundColor = 'white';
     }
-} 
+}
 
 //Update grid size based on Range Input 
-function ChangeGridSize(){
+function ChangeGridSize() {
 
     //get range input from document
     const size = RangeInput.value;
 
     //update output range with size
     document.getElementById('OutputRangeId').textContent = size;
-    
+
     //generate the new grid 
     CreateGrid(size);
 }
 
-//Function to save the drawing in clipboard
-function saveBoard(){
-    //create a canvas layout
+// Function to save the drawing in clipboard
+function saveBoard() {
+    
+    // Create a canvas layout
     const canvas = document.createElement('canvas');
 
-    //get size of container
+    // Get size of container
     const containerRect = container.getBoundingClientRect();
 
-    //set canvas dimensions to match the container
+    // Set canvas dimensions to match the container
     canvas.width = containerRect.width;
     canvas.height = containerRect.height;
 
-    //get the canvas context
+    // Get the canvas context
     const ctx = canvas.getContext('2d');
 
-    //Loop through each cell in the container
+    // Loop through each cell in the container
     const cells = container.getElementsByClassName('cell');
-
     for (let i = 0; i < cells.length; i++) {
 
         // Get the cell position and color
         const cellRect = cells[i].getBoundingClientRect();
 
-        //ghet the horizontal index position
+        // Get the horizontal index position
         const x = cellRect.left - containerRect.left;
 
-        //ghet the vertical index position
+        // Get the vertical index position
         const y = cellRect.top - containerRect.top;
+
+        //get color 
         const color = cells[i].style.backgroundColor;
 
-        // Draw the cell on the canvas
-        ctx.fillStyle = color;
+        // Check if color has a valid value, otherwise set it to white
+        if (!color) {
+            ctx.fillStyle = "white";
+        } else {
+            ctx.fillStyle = color;
+        }
+
         ctx.fillRect(x, y, cells[i].offsetWidth, cells[i].offsetHeight);
     }
+
+    // Convert canvas to a data URL
+    const dataURL = canvas.toDataURL('image/png');
+
+    // Create a temporary link element to download the image
+    const a = document.createElement('a');
+    a.href = dataURL;
+    a.download = 'Etch-a-Sketch.png';
+    a.target = '_blank';
+
+    // To download the image - trigger a link event on element a
+    a.dispatchEvent(new MouseEvent('click'));
+
+    // Open the image in a new window
+    window.open(dataURL, '_blank');
 }
